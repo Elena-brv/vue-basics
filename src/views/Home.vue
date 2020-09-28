17bd0646d21c37159b6d13a0f39b51201764f117
Elena-brv <leena.brv@gmail.com>
2020-09-29 00:06:13 +0300

    vue try

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Home page</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Ipsam asperiores quibusdam,
+      obcaecati rem ea assumenda voluptate. Fugiat
+      necessitatibus odit, aliquid assumenda minus
+      optio laborum voluptatem aliquam accusamus vitae
+      voluptates eum voluptatibus ducimus sed? Illo
+      incidunt quae molestias quisquam hic sed deserunt
+      architecto dicta placeat natus, aspernatur
+      beatae! Debitis, est tenetur!
+    </p>
+    <router-link to="/todos">Todos</router-link>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
   },
 };
 </script>
